e7df6d33ea1853e4f71d2c425ae6ad5b20fd975b
Fabian Jelinek <jeli2k@users.noreply.github.com>
2024-05-01 14:12:55 +0200

    added login/logout function, modified navbar

--- Frontend/js/controller.js
+++ Frontend/js/controller.js
@@ -6,13 +6,57 @@ $(document).ready(function () {
 
 function includeNavbar() {
     $.ajax({
-        url: 'components/navbar.html',
+        url: '../sites/navbar.php',
         type: 'GET',
         success: function (response) {
             $('#navbarContainer').html(response);
         },
-        error: function () {
-            console.error('Error loading navbar.');
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.error('Error loading navbar.', textStatus, errorThrown);
+        }
+    });
+}
+
+function login() {
+    var username = $('#username').val();
+    var password = $('#password').val();
+    var remember = $("#remember").is(":checked");
+
+    $.ajax({
+        url: '../../Backend/logic/login.php',
+        type: 'POST',
+        data: {
+            username: username,
+            password: password,
+            remember: remember
+        },
+        dataType: 'json',
+        success: function (response) {
+            if (response == 'success') {
+                window.location.href = '../sites/home.html';
+            } else {
+                alert('Login failed. Please try again.');
+            }
+        },
+        error: function (textStatus, errorThrown) {
+            console.error('Error logging in.', textStatus, errorThrown);
+        }
+    });
+}
+
+function logout() {
+    $.ajax({
+        url: '../../Backend/logic/logout.php',
+        type: 'GET',
+        success: function (response) {
+            if (response == 'success') {
+                window.location.href = '../sites/home.html';
+            } else {
+                alert('Logout failed. Please try again.');
+            }
+        },
+        error: function (textStatus, errorThrown) {
+            console.error('Error logging out.', textStatus, errorThrown);
         }
     });
 }
---
+++ Frontend/sites/navbar.php
@@ -0,0 +1,59 @@
+<?php session_start(); ?>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+      <a class="navbar-brand" href="home.html">WebShop</a>
+      <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarNav">
+    <!-- left side navbar -->
+          <div class="navbar-nav">
+        <!-- always available -->
+            <li class="nav-item">
+                <a class="nav-link" href="products.html">Products</a>
+            </li>
+            
+        <!-- user is logged in -->
+          <?php if (isset($_SESSION["username"]) || isset($_COOKIE["username"])): ?>
+            <li class="nav-item">
+                <a class="nav-link" href="home.html">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="orders.html">Orders</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="invoices.html">Invoices</a>
+            </li>
+          <?php endif; ?>
+          </div>
+    <!-- right side navbar -->
+          <div class="navbar-nav">
+            <li class="nav-item align-self-center">
+                <form class="form-inline d-flex">
+                    <input class="form-control form-control-sm mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success btn-sm my-2 my-sm-0" type="submit" style="margin-left: 5px;">Search</button>
+                </form>
+            </li>
+        <!-- user is logged in -->
+          <?php if (isset($_SESSION["username"]) || isset($_COOKIE["username"])): ?>
+            <li class="nav-item">
+                <a class="nav-link" href="../../Backend/logic/logout.php">Logout</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="account.html">My Account</a>
+            </li>
+          <?php endif; ?>
+        <!-- always available -->
+          <li class="nav-item">
+                <a class="nav-link" href="#">Cart</a>
+          </li>
+        <!-- user is not logged in -->
+          <?php if (!isset($_SESSION["username"]) && !isset($_COOKIE["username"])): ?>
+            <li class="nav-item">
+                <a class="nav-link" href="login.html">Login</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="register.html">Register</a>
+            </li>
+          <?php endif; ?>
+          </div>
+      </div>
+  </div>
+</nav>
