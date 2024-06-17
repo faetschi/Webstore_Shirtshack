7a979ecc114ddeb08df1964177131767fcfdce30
Fabian Jelinek <jeli2k@users.noreply.github.com>
2024-06-17 14:15:50 +0200

    adjusted README

--- Backend/config/dbaccess.php
+++ Backend/config/dbaccess.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "ShirtShack";
+$servername = "localhost";  // if you are using XAMPP or MAMPP, this should be localhost
+$username = "root";         // change for your XAMPP/MAMPP configuration
+$password = "";             // change for your XAMPP/MAMPP configuration
+$dbname = "ShirtShack";     // change to your db name
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
